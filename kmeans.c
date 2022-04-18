a54335b1b3b5bcdf958d198ed6077aedfa568767
GalJakob <galj@mail.tau.ac.il>
2022-04-18 19:58:35 +0300

    points

--- kmeans.c
+++ kmeans.c
@@ -1,37 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-void assignVars(int,int,char*);
+#include <string.h>
+
+#define LINE_LENGTH 256
+
+void assignVars(int *, int *, char **, char **, char **, int);
+void assignPoints(int *, char **);
 
 int main(int argc, char *argv[])
 {
+    int K;
+    int max_iter;
+    int pointsArr[1];
+    char *inFileNamePtr;
+    char *outFileNamePtr;
 
-    /* with max_iter */
-    char c;
-    FILE* filePtr;
-    int K = atoi(argv[1]);
-    int max_iter = atoi(argv[2]);
-    char **fileNamePtr;
-    fileNamePtr = &argv[3];
+    assignVars(&K, &max_iter, &inFileNamePtr, &outFileNamePtr, argv, argc);
+    assignPoints(pointsArr, &inFileNamePtr);
 
-    filePtr = fopen(*fileNamePtr,"r");
-    c = fgetc(filePtr);
-    printf("asa %c",c);
-    K++;
-    max_iter++;
-    fclose(filePtr);
-
-    /* char** ptr = *argv[3];
-    printf("%s",*ptr); */
-
-    printf("number of arguments: %d", argc - 1);
-    /*
-    printf("k : %s\n", argv[1]);
-
-    printf("max_iter : %s\n", argv[2]);
-    printf("file name : %s\n", argv[3]);
-
-    ptr = fopen("test.txt", "r");
- */
+    printf("%d", pointsArr[0]);
     return 0;
+}
+
+/* functions :*/
+
+void assignVars(int *kPtr, int *max_iterPtr, char **inFileNamePtr, char **outFileNamePtr, char **args, int numOfArgs)
+{
+    if (numOfArgs == 5)
+    {
+        *kPtr = atoi(args[1]);
+        *max_iterPtr = atoi(args[2]);
+        *inFileNamePtr = args[3];
+        *outFileNamePtr = args[4];
+    }
+    else
+    {
+        *kPtr = atoi(args[1]);
+        *inFileNamePtr = args[2];
+        *outFileNamePtr = args[3];
+    }
+}
+
+void assignPoints(int *pointArrPtr, char **inFileNamePtr)
+{
+    char line[LINE_LENGTH];
+    char *splittedLine;
+    FILE *filePtr;
+
+    filePtr = fopen(*inFileNamePtr, "r");
+
+    while (fgets(line, LINE_LENGTH, filePtr))
+    {
+        splittedLine = strtok(line,",");
+        printf("%s\n", splittedLine);
+    }
+    fclose(filePtr);
+}
+
+/* @pre:string must not contain letters */
+int convertStrToNumber(char *strToConvert){
+    
 }
