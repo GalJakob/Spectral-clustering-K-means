375b30408e12cf4ce844376dd60a90642b08244e
noa-barak97 <92519079+noa-barak97@users.noreply.github.com>
2022-04-22 01:07:30 +0300

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -5,16 +5,10 @@ n = len(sys.argv)
 if n < 4:
   sys.exit("Invalid Input")
 
-<<<<<<< HEAD
 k = sys.argv[1]
 try:
   k = float(k)
 except ValueError:
-=======
-k = sys.argv[0]
-k_num = k.split(".")
-if len(k_num) > 2:
->>>>>>> b7078550b342b5dfd0d61731484729efce98f222
   sys.exit("Invalid Input!")
 
 if float(k) < 1 or float(k) != int(k):
@@ -90,47 +84,44 @@ while (iter_cnt != max_iter) and (Euclidean_norm == False):
 
       for l in range(point_len):
         temp_dis += math.pow((float(point[l]) - float(centroids_arr[c][l])), 2)
-        print(point[l])
-        print(centroids_arr[c][l])
+
       if temp_dis < min_dis:
-        cluster=c
-        min_dis=temp_dis
+        cluster = c
+        min_dis = temp_dis
 
     for v in range(point_len):
       c_sum[cluster][v] += float(point[v])
 
     c_size[cluster] += 1
 
-  norm=[]
+  norm = []
 
   for i in range(k):
-    old_centroid=centroids_arr[i]
+    old_centroid = centroids_arr[i]
     try:
       new_centroid = [str(c_sum[i][j]/c_size[i]) for j in range(point_len)]
     except ZeroDivisionError:
       sys.exit("An Error Has Occurred")
-    centroids_arr[i]=new_centroid
-    distance=0
+    centroids_arr[i] = new_centroid
+    distance = 0
 
     for l in range(point_len):
-      distance += math.pow((float(old_centroid[l]) -
-                           float(new_centroid[l])), 2)
+      distance += math.pow((float(old_centroid[l]) - float(new_centroid[l])), 2)
 
     norm.append(distance)
 
-  norm=[math.sqrt(i) for i in norm]
-  Euclidean_norm=True
+  norm = [math.sqrt(i) for i in norm]
+  Euclidean_norm = True
 
   for i in range(k):
 
     if norm[i] > epsilon:
-      Euclidean_norm=False
+      Euclidean_norm = False
       continue
 
   iter_cnt += 1
 
-centroids_arr=[[str('%.4f' % (float(centroids_arr[i][j])))
-                    for j in range(point_len)] for i in range(k)]
+centroids_arr = [[str('%.4f' % (float(centroids_arr[i][j]))) for j in range(point_len)] for i in range(k)]
 
 try:
   res = open(output_filename, "w")
@@ -138,7 +129,7 @@ except IOError:
   sys.exit("An Error Has Occurred")
 
 for i in range(k):
-  final_line=centroids_arr[i][0]
+  final_line = centroids_arr[i][0]
   for j in range(1, point_len):
     final_line += "," + centroids_arr[i][j]
 
