fb96b40864ca9c61e79b7f9d3c2daa8067b803de
GalJakob <galj@mail.tau.ac.il>
2022-04-21 16:22:57 +0300

    s

--- kmeans.c
+++ kmeans.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
+#include <math.h>
 
 #define LINE_LENGTH 256
+#define DEFAULT_MAX_ITER 10000
+#define EPSILON 0.001
 #define PERCISION 10000
 
 void assignVars(int *, int *, char **, char **, char **, int);
 void assignPoints(double ***, char **, int *, int *);
 void computeNumOfCordsAndPoints(FILE **, int *, int *, char ***);
-void initializeCentroids(double ***, double ***, int, int, int);
-void mainAlgorithm(double ***, double ***);
+void initializeCentroids(double ***, double ***, int,int);
+void mainAlgorithm(double ***, double ***, int, int, int, int);
 
 int main(int argc, char *argv[])
 {
     int K;
-    int max_iter;
+    int max_iter = DEFAULT_MAX_ITER;
     int numOfPoints;
     int numOfCords;
     double **pointsArr;
@@ -26,8 +28,8 @@ int main(int argc, char *argv[])
     /* reminder:arr == &arr[0] :*/
     assignVars(&K, &max_iter, &inFileNamePtr, &outFileNamePtr, argv, argc);
     assignPoints(&pointsArr, &inFileNamePtr, &numOfPoints, &numOfCords);
-    initializeCentroids(&pointsArr, &centroids, K, numOfPoints, numOfCords);
-    mainAlgorithm(&pointsArr, &centroids);
+    initializeCentroids(&pointsArr, &centroids, K,numOfCords);
+    mainAlgorithm(&pointsArr, &centroids, max_iter, numOfPoints, numOfCords, K);
 
     return 0;
 }
@@ -57,7 +59,6 @@ void assignPoints(double ***pointArrPtr, char **inFileNamePtr, int *numOfCordsAr
     char *splittedLine;
 
     double cordinateVal;
-    double *pointPtr;
     int numOfCords = 0;
     int numOfCordsIdx = 0;
     int numOfPoints = 0;
@@ -116,7 +117,7 @@ void computeNumOfCordsAndPoints(FILE **filePtr, int *numOfCords, int *numOfPoint
     fclose(*filePtr);
 }
 
-void initializeCentroids(double ***pointArrPtr, double ***centroidsPtr, int K, int numOfCords, int numOfPoints)
+void initializeCentroids(double ***pointArrPtr, double ***centroidsPtr, int K, int numOfCords)
 {
     int pointIdx;
     int cord;
@@ -128,24 +129,68 @@ void initializeCentroids(double ***pointArrPtr, double ***centroidsPtr, int K, i
         for (cord = 0; cord < numOfCords; cord++)
             (*centroidsPtr)[pointIdx][cord] = (*pointArrPtr)[pointIdx][cord];
     }
-
-    printf("%f", (*centroidsPtr)[K - 1][2]);
 }
 void output(double ***centroidsPtr, int k, int numOfCords, char **outFileNamePtr)
 {
-    FILE *res;
-    res = fopen(*outFileNamePtr, "w");
-    int i = 0;
-    for (; i < k; i++)
-     {
-        fprintf(res, "%.4f", *centroidsPtr[i][0]);
-        int j = 1;
-        for (; j < numOfCords - 1; j++){
-        }
-     }
+    /*  FILE *res;
+     res = fopen(*outFileNamePtr, "w");
+     int i = 0;
+     for (; i < k; i++)
+      {
+         fprintf(res, "%.4f", *centroidsPtr[i][0]);
+         int j = 1;
+         for (; j < numOfCords - 1; j++){
+         }
+      } */
 }
 
-void mainAlgorithm(double ***pointsArrPtr, double ***centroidsArrPtr)
+void mainAlgorithm(double ***pointsArrPtr, double ***centroidsArrPtr, int max_iter, int numOfPoints, int numOfCords, int K)
 {
+    int iterCnt = 1;
+    int isAllEuclidiansUnderEps = 0; /*boolean*/
+    int pointIdx = 0;
+    int pointIdxForInitCluster;
+    int cordForCluster;
 
+    double **clustersSumArrPtr;
+    int *numOfPointsInCluster = (int *)malloc(K * sizeof(int));
+
+
+    clustersSumArrPtr = (double **)malloc((sizeof(double *)) * K);
+    for (pointIdxForInitCluster = 0; pointIdxForInitCluster < K; pointIdxForInitCluster++)
+    {
+        clustersSumArrPtr[pointIdxForInitCluster] = (double *)malloc(numOfCords * sizeof(double));
+        for (cordForCluster = 0; cordForCluster < numOfCords; cordForCluster++)
+           clustersSumArrPtr[pointIdxForInitCluster][cordForCluster] = 0;
+    }
+    /* numOfPointsInCluster[1] = 2;
+    printf("%p \n",numOfPointsInCluster);
+    printf("%p",numOfPointsInCluster+1); */
+    while ((iterCnt != max_iter) && (!isAllEuclidiansUnderEps))
+    {
+        for (pointIdx = 0; pointIdx < numOfPoints; pointIdx++)
+        {
+            double* point = (*pointsArrPtr)[pointIdx];
+            float minDistance = INFINITY;
+            int chosenClusterIdx = 0;
+            int clusterIdx = 0;
+
+            for (clusterIdx = 0; clusterIdx < K; clusterIdx++)
+            {
+                float tempDist =0;
+                for (size_t i = 0; i < count; i++)
+                {
+                    /* code */
+                }
+                
+                for l in range(point_len):
+        temp_dis += math.pow((float(point[l]) - float(centroids_arr[c][l])), 2)
+                /* code */
+            }
+            
+
+        }
+    }
+
+    /*  point_len = len(datapoints_arr[0]) */
 }
