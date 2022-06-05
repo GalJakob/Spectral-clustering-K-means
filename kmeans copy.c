57b7223deabf0451874f29b902b372d82fd53ba8
GalJakob <galj@mail.tau.ac.il>
2022-06-05 22:19:54 +0300

    finish no tests

--- kmeans copy.c
+++ kmeans copy.c
@@ -10,10 +10,198 @@
 #define DEFAULT_MAX_ITER 200
 #define EPSILON 0.001
 
-static PyObject *funcFromPy(PyObject *self, PyObject *args);
+void assignVars(int *, int *, char **, char **, char **, int, int *);
+void assignPoints(double ***, char **, int *, int *, int *, int);
+void computeNumOfCordsAndPoints(FILE **, int *, int *, char ***, int **);
+void initializeCentroids(double ***, double ***, int, int, int *);
 PyObject *mainAlgorithm2(double ***, double ***, int, int, int, int);
-PyObject *convertDPToPyObj(double ***, int, int);
+void output(double ***, int, int, char **, int *);
 double **convertPyObjToDP(PyObject *, int, int);
+PyObject *convertDPToPyObj(double ***, int, int);
+
+/* int main(int argc, char *argv[])
+{
+    int K;
+    int shouldTerminate = 0;
+    int max_iter = DEFAULT_MAX_ITER;
+    int numOfPoints;
+    int numOfCords;
+    double **pointsArr;
+    double **centroids;
+    char *inFileNamePtr;
+    char *outFileNamePtr;
+
+    assignVars(&K, &max_iter, &inFileNamePtr, &outFileNamePtr, argv, argc, &shouldTerminate);
+    assignPoints(&pointsArr, &inFileNamePtr, &numOfPoints, &numOfCords, &shouldTerminate, K);
+    initializeCentroids(&pointsArr, &centroids, K, numOfCords, &shouldTerminate);
+    mainAlgorithm(&pointsArr, &centroids, max_iter, numOfPoints, numOfCords, K);
+    output(&centroids, K, numOfCords, &outFileNamePtr, &shouldTerminate);
+
+    return shouldTerminate;
+}
+*/
+
+/* functions :*/
+
+void assignVars(int *kPtr, int *max_iterPtr, char **inFileNamePtr, char **outFileNamePtr, char **args, int numOfArgs, int *shouldTerminate)
+{
+
+    if (numOfArgs == 5)
+    {
+        *kPtr = atoi(args[1]);
+        *max_iterPtr = atoi(args[2]);
+        *inFileNamePtr = args[3];
+        *outFileNamePtr = args[4];
+
+        if (*kPtr == 0 || *max_iterPtr == 0)
+        {
+            *shouldTerminate = 1;
+            printf("%s", "Invalid input!");
+            return;
+        }
+    }
+    else if (numOfArgs == 4)
+    {
+        *kPtr = atoi(args[1]);
+        *inFileNamePtr = args[2];
+        *outFileNamePtr = args[3];
+    }
+    else
+    {
+        *shouldTerminate = 1;
+        printf("%s", "Invalid input!");
+        return;
+    }
+}
+
+void assignPoints(double ***pointArrPtr, char **inFileNamePtr, int *numOfPointsArg, int *numOfCordsArg, int *shouldTerminate, int K)
+{
+    char line[LINE_LENGTH];
+    char *splittedLine;
+
+    double cordinateVal;
+    int numOfCords = 0;
+    int numOfCordsIdx = 0;
+    int numOfPoints = 0;
+    int numOfPointsIdx = 0;
+
+    FILE *filePtr;
+    if (*shouldTerminate)
+        return;
+    computeNumOfCordsAndPoints(&filePtr, &numOfCords, &numOfPoints, &inFileNamePtr, &shouldTerminate);
+    *numOfCordsArg = numOfCords;
+    *numOfPointsArg = numOfPoints;
+    if (K >= numOfPoints)
+        printf("%s", "Invalid input!");
+
+    *pointArrPtr = (double **)malloc((sizeof(double *)) * numOfPoints);
+    if (*shouldTerminate || pointArrPtr == NULL)
+    {
+        printf("%s", "An Error Has Occurred ");
+        *shouldTerminate = 1;
+        return;
+    }
+    filePtr = fopen(*inFileNamePtr, "r");
+    while (fgets(line, LINE_LENGTH, filePtr))
+    {
+        (*pointArrPtr)[numOfPointsIdx] = (double *)malloc(numOfCords * sizeof(double));
+
+        for (numOfCordsIdx = 0; numOfCordsIdx < numOfCords; numOfCordsIdx++)
+        {
+            if (numOfCordsIdx == 0)
+                splittedLine = strtok(line, ",");
+            else
+                splittedLine = strtok(NULL, ",");
+            cordinateVal = atof(splittedLine);
+            (*pointArrPtr)[numOfPointsIdx][numOfCordsIdx] = cordinateVal;
+        }
+        numOfPointsIdx++;
+        numOfCordsIdx = 0;
+    }
+    fclose(filePtr);
+}
+
+void computeNumOfCordsAndPoints(FILE **filePtr, int *numOfCords, int *numOfPoints, char ***inFileNamePtr, int **shouldTerminate)
+{
+    char line[LINE_LENGTH];
+    char *splittedLine;
+    *filePtr = fopen(**inFileNamePtr, "r");
+    if (*filePtr == NULL)
+    {
+        **shouldTerminate = 1;
+        printf("%s", "An Error Has Occurred");
+        return;
+    }
+
+    while (fgets(line, LINE_LENGTH, *filePtr))
+    {
+        *numOfPoints = *numOfPoints + 1;
+        if (*numOfCords != 0)
+            continue;
+        else
+        {
+            do
+            {
+                if (*numOfCords == 0)
+                    splittedLine = strtok(line, ",");
+                else
+                    splittedLine = strtok(NULL, ",");
+                *numOfCords = *numOfCords + 1;
+            } while (splittedLine != NULL);
+            *numOfCords = *numOfCords - 1;
+        }
+    }
+    if (fclose(*filePtr) != 0)
+    {
+        **shouldTerminate = 1;
+        printf("%s", "An Error Has Occurred");
+        return;
+    }
+    free(splittedLine);
+}
+
+void initializeCentroids(double ***pointArrPtr, double ***centroidsPtr, int K, int numOfCords, int *shouldTerminate)
+{
+
+    int pointIdx = 0;
+    int cord = 0;
+    if (*shouldTerminate)
+        return;
+    *centroidsPtr = (double **)malloc((sizeof(double *)) * K);
+    if (centroidsPtr == NULL)
+    {
+        *shouldTerminate = 1;
+        printf("%s", "An Error Has Occurred");
+        return;
+    }
+    for (pointIdx = 0; pointIdx < K; pointIdx++)
+    {
+        (*centroidsPtr)[pointIdx] = (double *)malloc(numOfCords * sizeof(double));
+        for (cord = 0; cord < numOfCords; cord++)
+            (*centroidsPtr)[pointIdx][cord] = (*pointArrPtr)[pointIdx][cord];
+    }
+}
+void output(double ***centroidsPtr, int k, int numOfCords, char **outFileNamePtr, int *shouldTerminate)
+{
+    FILE *res;
+    int i = 0;
+    if (*shouldTerminate)
+        return;
+    res = fopen(*outFileNamePtr, "w");
+
+    for (; i < k; i++)
+    {
+        int j = 0;
+        for (; j < numOfCords - 1; j++)
+        {
+            fprintf(res, "%.4f", (*centroidsPtr)[i][j]);
+            fprintf(res, "%s", ",");
+        }
+        fprintf(res, "%.4f", (*centroidsPtr)[i][j]);
+        fprintf(res, "\n");
+    }
+    fclose(res);
+}
 
 PyObject *mainAlgorithm2(double ***pointsArrPtr, double ***centroidsArrPtr, int max_iter, int numOfPoints, int numOfCords, int K)
 {
