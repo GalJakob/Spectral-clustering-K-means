7a7175db229cebb5550300b3745a4e7a316fc54f
noa-barak97 <noabarak2@mail.tau.ac.il>
2022-08-25 00:14:03 +0300

    n

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -14,6 +14,44 @@ static PyObject *execByGoalFromPy(PyObject *self, PyObject *args);
 
 /* ########## gal ########## */
 
+static PyObject* createPythonMat(double **matC, int size, int cords){
+    int i,j;
+    PyObject *temp;
+    PyObject *matP;
+    matP = PyList_New(size);
+
+    for (i = 0; i < size; i++){
+        temp = PyList_New(cords);
+
+        for (j = 0; j < cords; j++){
+            PyList_SetItem(temp, j, PyFloat_FromDouble(matC[i][j]));
+        }
+        PyList_SetItem(matP, i, temp);
+    }
+    return matP;
+}
+
+static double** createCMat(PyObject *matP, int size, int cords){
+    int i, j;
+    PyObject *V;
+    PyObject *cord;
+    double ** matC = (double **)calloc(size, sizeof(double *));
+    assert(matC != NULL);
+    for (i = 0; i < size; i++){
+        matC[i] = (double *)calloc(cords, sizeof(double));
+        assert(matC[i] != NULL);
+        V = PyList_GetItem(matP, (Py_ssize_t)i);
+        for (j = 0; j < cords; j++){
+            cord = PyList_GetItem(V, (Py_ssize_t)j);
+            matC[i][j] = PyFloat_AsDouble(cord);
+        }
+    }
+    return matC;
+}
+
+
+
+// don't know if we need this. plz check :)
 static PyObject *execByGoalFromPy(PyObject *self, PyObject *args)
 {
     
